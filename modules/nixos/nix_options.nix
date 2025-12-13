6570e83c4e0b78451f22ff694b1a9c8cd607493f
pabotesu <pab0te2u@gmail.com>
2025-12-13 17:14:43 +0900

    usenameの指定を明確にする

--- modules/nixos/nix_options.nix
+++ modules/nixos/nix_options.nix
@@ -1,9 +1,9 @@
-{username, ...}: {
+{userConfig, ...}: {
   nix = {
     settings = {
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
-      trusted-users = ["root" "${username}"];
+      trusted-users = ["root" "${userConfig.name}"];
       accept-flake-config = true;
     };
     gc = {
