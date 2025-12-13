1c4bc6953d730552a8418f74c3cc53b264e8f18f
pabotesu <pab0te2u@gmail.com>
2025-12-13 17:09:42 +0900

    inputとsoundの修正

--- modules/nixos/inputmethod.nix
+++ modules/nixos/inputmethod.nix
@@ -1,8 +1,9 @@
 {pkgs, ...}: {
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [
-      fcitx5-configtool
+      qt6Packages.fcitx5-configtool
       fcitx5-mozc
       fcitx5-gtk
       fcitx5-skk
