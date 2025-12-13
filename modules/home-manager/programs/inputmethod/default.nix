9176c21b6a496598fb6deb4cfbc20d20b363bb92
pabotesu <pab0te2u@gmail.com>
2025-12-13 22:08:29 +0900

    mozcの設定を行う

--- modules/home-manager/programs/inputmethod/default.nix
+++ modules/home-manager/programs/inputmethod/default.nix
@@ -7,4 +7,36 @@
       fcitx5-gtk
     ];
   };
+
+  # fcitx5 configuration
+  home.file.".config/fcitx5/profile".text = ''
+    [Groups/0]
+    Name=Default
+    Default Layout=us
+    DefaultIM=mozc
+
+    [Groups/0/Items/0]
+    Name=keyboard-us
+    Layout=
+
+    [Groups/0/Items/1]
+    Name=mozc
+    Layout=
+
+    [GroupOrder]
+    0=Default
+  '';
+
+  home.file.".config/fcitx5/config".text = ''
+    [Hotkey]
+    TriggerKeys=Control+space
+    EnumerateWithTriggerKeys=True
+    EnumerateForwardKeys=
+    EnumerateBackwardKeys=
+    EnumerateSkipFirst=False
+
+    [Behavior]
+    ActiveByDefault=False
+    ShareInputState=No
+  '';
 }
