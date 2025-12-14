051c8dc4412882c759ae98ceb66c8c4837cd24f9
pabotesu <pab0te2u@gmail.com>
2025-12-14 14:17:47 +0900

    themeを全体的に適応する

--- modules/home-manager/programs/wayland/hyprland.config.nix
+++ modules/home-manager/programs/wayland/hyprland.config.nix
@@ -76,7 +76,7 @@ general {
     gaps_in = 5
     gaps_out = 5
     border_size = 2
-    col.active_border = rgba(${envColors.blue}ee) rgba(${envColors.cyan}ee) 45deg
+    col.active_border = rgba(${envColors.fg}ee) rgba(${envColors.blue}ee) 45deg
     col.inactive_border = rgba(${envColors.black}aa)
 
     layout = dwindle
