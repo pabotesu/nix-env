262472b754eb9fc8c023642be57e439271740043
pabotesu <pab0te2u@gmail.com>
2025-12-14 16:34:33 +0900

    1passの導入

--- modules/home-manager/programs/secrets/default.nix
+++ modules/home-manager/programs/secrets/default.nix
@@ -1,7 +1,9 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    _1password-gui
-    _1password
-    # age # 将来的に秘密管理用に追加予定
-  ];
+{pkgs, lib, ...}: {
+  home.packages = with pkgs; 
+    lib.optionals pkgs.stdenv.isLinux [
+      _1password-gui
+      _1password
+    ] ++ [
+      # age # 将来的に秘密管理用に追加予定（Linux/Darwin両方）
+    ];
 }
