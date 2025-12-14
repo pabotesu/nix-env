8206a9b84de6a3f81c76ef39bea94e3f0298d86b
pabotesu <pab0te2u@gmail.com>
2025-12-14 13:26:23 +0900

    通知用のフォントを少し小さくする

--- modules/home-manager/programs/mako/default.nix
+++ modules/home-manager/programs/mako/default.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       default-timeout = 5000;
-      font = "HackGen35 Console NFJ";
+      font = "HackGen35 Console NFJ 11";
       border-radius = 10;
       border-size = 2;
       background-color = envTheme.bg;
