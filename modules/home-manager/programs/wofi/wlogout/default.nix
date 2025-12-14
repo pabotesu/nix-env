a7e96f3ba59d8255277f390c9139861a916248ab
pabotesu <pab0te2u@gmail.com>
2025-12-14 13:35:22 +0900

    wlogoutの編集

--- modules/home-manager/programs/wofi/wlogout/default.nix
+++ modules/home-manager/programs/wofi/wlogout/default.nix
@@ -26,6 +26,39 @@
           text = "Reboot";
           keybind = "r";
         }
+        {
+          label = "suspend";
+          action = "systemctl suspend";
+          text = "Suspend";
+          keybind = "u";
+        }
       ];
+      style = ''
+        window {
+          background-color: rgba(29, 31, 33, 0.95);
+        }
+        
+        button {
+          background-color: rgba(40, 44, 52, 0.8);
+          border-radius: 10px;
+          border: 2px solid rgba(197, 200, 198, 0.3);
+          margin: 15px;
+          background-repeat: no-repeat;
+          background-position: center;
+          background-size: 25%;
+          color: #c5c8c6;
+          font-size: 16px;
+        }
+        
+        button:hover {
+          background-color: rgba(60, 64, 72, 0.9);
+          border: 2px solid rgba(197, 200, 198, 0.6);
+        }
+        
+        button:focus {
+          background-color: rgba(80, 84, 92, 1);
+          border: 2px solid #c5c8c6;
+        }
+      '';
   };
 }
